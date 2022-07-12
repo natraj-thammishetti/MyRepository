15324c8f29f31ff37fe9d7e9272b2c32fc7d44fa
natraj-thammishetti <94598441+natraj-thammishetti@users.noreply.github.com>
2022-07-12 15:09:04 +0530

    Add files via upload

---
+++ ec2_instance_in_last_24_hours.py
@@ -0,0 +1,31 @@
+import boto3
+from datetime import datetime, timezone
+
+
+def instances_in_last_24_hours():
+    try:
+        # Initializing ec2 client using boto3 module
+        ec2_resource = boto3.client("ec2")
+        # describe_instance() method returns the dictionary that contains the details of ec2 instances
+        instance_details = ec2_resource.describe_instances()["Reservations"]
+        instances = {}
+        for instance in instance_details:
+            instances[instance['Instances'][0]['InstanceId']] = {}
+            instances[instance['Instances'][0]['InstanceId']]['launch_time'] = instance['Instances'][0]['LaunchTime']
+            instances[instance['Instances'][0]['InstanceId']]['state'] = instance['Instances'][0]['State']['Name']
+
+        print("List of Instances created in last 24 hours :")
+        # printing the instances that were created in less than 24 hours
+        count = 0
+        for k, v in instances.items():
+            time_diff = datetime.now(timezone.utc)-v['launch_time']
+            hours = time_diff.total_seconds()//(60*60)
+            if hours < 24:
+                print("Id :", k, "- Time :", v['launch_time'], "- State :", v['state'])
+                count += 1
+        print("The Number of Instances created in last 24 hours :", count)
+    except Exception as e:
+        print(e)
+
+
+instances_in_last_24_hours()
