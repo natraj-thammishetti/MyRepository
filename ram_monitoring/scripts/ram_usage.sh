5de0fbed43a5af8bf7b605128d691128d314c174
natraj-thammishetti <94598441+natraj-thammishetti@users.noreply.github.com>
2022-07-22 14:57:33 +0530

    Update ram_usage.sh

--- ram_monitoring/scripts/ram_usage.sh
+++ ram_monitoring/scripts/ram_usage.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 used_percent=$(free | awk '/Mem/{printf("RAM Usage: %.2f%\n"), ($2-$7)/$2*100}' | awk '{print $3}' | cut -d"." -f1)
 echo "The Memory Usage is $used_percent%"
-if [ $used_percent -ge 90 ]; then
+if [ $used_percent -ge 80 ]; then
 echo "CRITICAL::Running out of Memory...The Usage is ($used_percent%)\" on $(hostname -i) $(hostname -f) <br>"
 else
-if [ $used_percent -ge 80 ]; then
+if [ $used_percent -ge 70 ]; then
  echo "WARNING:Running out of Memory...The Usage is ($used_percent%)\" on $(hostname -i) $(hostname -f) <br>"
 fi
 fi
