c72f74bf4915a0e50efb81c5d542d4b0b39dac44
natraj-thammishetti <94598441+natraj-thammishetti@users.noreply.github.com>
2022-07-22 12:25:01 +0530

    Update memory_usage.py

--- ram_monitoring/scripts/memory_usage.py
+++ ram_monitoring/scripts/memory_usage.py
@@ -7,48 +7,48 @@ import sys
 
 
 def get_ram_usage(region, key_name, pem_file_path, exclusion_list, scripts_path):
-    print("Hello")
+#    print("Hello")
     EC2 = boto3.client('ec2', region)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    print("Hai")
-#    reservations = EC2.describe_instances()["Reservations"]
-#    print(reservations)
-#     path = pem_file_path
-#     privkey = paramiko.RSAKey.from_private_key_file(path)
-#     for reservation in reservations:
-#         for instance in reservation["Instances"]:
-#             ip = instance['PrivateIpAddress']
-#             instance_id = instance['InstanceId']
-#             print(ip + ":" + instance_id)
-#             list = exclusion_list
-#             if instance_id not in list:
-#                 try:
-#                     ssh.connect(hostname=ip, username='cloudbreak', pkey=privkey)
-#                     ftp_client = ssh.open_sftp()
-#                     ftp_client.put(scripts_path + '/ram_usage.sh', '/tmp/stats.sh')
-#                     ftp_client.close()
-#                     stdin, stdout, stderr = ssh.exec_command("bash /tmp/stats.sh")
-#                 except paramiko.AuthenticationException:
-#                     try:
-#                         ssh.connect(hostname=ip, username='ec2-user', pkey=privkey)
-#                         ftp_client = ssh.open_sftp()
-#                         ftp_client.put(scripts_path + '/ram_usage.sh', '/tmp/stats.sh')
-#                         ftp_client.close()
-#                         stdin, stdout, stderr = ssh.exec_command("bash /tmp/stats.sh")
-#                     except paramiko.AuthenticationException:
-#                         print('Authentication failed when connecting to' + ip)
-#                     except TimeoutError:
-#                         print('Connection timed out for:' + ip + ":" + instance_id)
-#                 except TimeoutError:
-#                     print('Connection timed out for:' + ip + ":" + instance_id)
+#    print("Hai")
+   reservations = EC2.describe_instances()["Reservations"]
+   print(reservations)
+    path = pem_file_path
+    privkey = paramiko.RSAKey.from_private_key_file(path)
+    for reservation in reservations:
+        for instance in reservation["Instances"]:
+            ip = instance['PrivateIpAddress']
+            instance_id = instance['InstanceId']
+            print(ip + ":" + instance_id)
+            list = exclusion_list
+            if instance_id not in list:
+                try:
+                    ssh.connect(hostname=ip, username='cloudbreak', pkey=privkey)
+                    ftp_client = ssh.open_sftp()
+                    ftp_client.put(scripts_path + '/ram_usage.sh', '/tmp/stats.sh')
+                    ftp_client.close()
+                    stdin, stdout, stderr = ssh.exec_command("bash /tmp/stats.sh")
+                except paramiko.AuthenticationException:
+                    try:
+                        ssh.connect(hostname=ip, username='ec2-user', pkey=privkey)
+                        ftp_client = ssh.open_sftp()
+                        ftp_client.put(scripts_path + '/ram_usage.sh', '/tmp/stats.sh')
+                        ftp_client.close()
+                        stdin, stdout, stderr = ssh.exec_command("bash /tmp/stats.sh")
+                    except paramiko.AuthenticationException:
+                        print('Authentication failed when connecting to' + ip)
+                    except TimeoutError:
+                        print('Connection timed out for:' + ip + ":" + instance_id)
+                except TimeoutError:
+                    print('Connection timed out for:' + ip + ":" + instance_id)
 
-#                 output = stdout.read().decode()
-#                 stdin.close()
-#                 print(output.strip('\n'))
-#                 print("")
-#                 print("----------------------------------------------------------------------------")
-#                 print("")
+                output = stdout.read().decode()
+                stdin.close()
+                print(output.strip('\n'))
+                print("")
+                print("----------------------------------------------------------------------------")
+                print("")
 
 
 if __name__ == "__main__":
